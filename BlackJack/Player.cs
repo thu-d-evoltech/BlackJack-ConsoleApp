8c396b210851ec17557cc1522cd493f7cebca30a
thu-d <thu-d@evoltech.co.jp>
2024-05-02 13:44:30 +0900

    Playerクラスを作成

--- BlackJack/Player.cs
+++ BlackJack/Player.cs
@@ -8,5 +8,27 @@ namespace BlackJack
 {
     internal class Player
     {
+        public List<Card> Hand { get; set; }
+
+        public bool Turn { get; set; } = true;
+
+        public Player()
+        {
+            //プレイヤーは最大5枚のカードを持る
+            Hand = new List<Card>(5);
+        }
+
+        //プレイヤーの持っているカードの合計
+        public int GetTotal()
+        {
+            int total = 0;
+            foreach (Card card in Hand)
+            {
+                total += card.FaceValue;
+                Console.WriteLine($"あなたの現在の得点は{total}です。");
+            }
+            return total;
+
+        }
     }
 }
