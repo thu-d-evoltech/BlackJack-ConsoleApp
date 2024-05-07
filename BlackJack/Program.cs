29bf108470e38127dd3485f81b9b949a7fb4ccdf
thu-d <thu-d@evoltech.co.jp>
2024-05-07 16:38:15 +0900

    コンソールを飾る

--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -3,9 +3,13 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("ブラックジャックへようこそ！");
+        Console.WriteLine("☆ ★ ☆ ★ ☆ ★ ☆ ★  ブラックジャックへようこそ！ ☆ ★ ☆ ★ ☆ ★ ☆ ★");
+        Console.WriteLine("");
 
         PlayGame playGame = new PlayGame();
         playGame.GetPlay();
+
+        Console.WriteLine("------------------------------");
+        Console.WriteLine("ブラックジャック終了！また遊んでね★");
     }
 }
