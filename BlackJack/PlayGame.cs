837b339bd9ff090a043fdf0153c46ecaca928bc5
thu-d <thu-d@evoltech.co.jp>
2024-05-07 10:30:41 +0900

    PlayGameクラスの作成

---
+++ BlackJack/PlayGame.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    internal class PlayGame
+    {
+        private Deck deck;
+        private Player player;
+        private Player dealer;
+
+        public PlayGame()
+        {
+            deck = new Deck();
+            deck.Shuffle();
+
+            player = new Player();
+            dealer = new Player();
+        }
+    }
+}
