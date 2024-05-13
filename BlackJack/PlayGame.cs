9c34f604ef0d72992fdc773793920842f21dcc3d
thu-d <thu-d@evoltech.co.jp>
2024-05-13 11:29:27 +0900

    Revert "ゲームのルールによって、コーデを修正"
    This reverts commit 687e8f785b0ae49d70f4de8d7297c4b3b80e6bc9.

--- BlackJack/PlayGame.cs
+++ BlackJack/PlayGame.cs
@@ -45,7 +45,7 @@ namespace BlackJack
             Program.Sleep();
 
             GetDealerTurn();
-            Console.WriteLine("\nゲームが完了しました\n");
+            Console.WriteLine("ゲームが完了しました\n");
 
             Program.Sleep();
             Console.WriteLine("結果");
@@ -100,18 +100,12 @@ namespace BlackJack
         public void GetDealerTurn()
         {
             Console.WriteLine("あなたの番が終了したのでディーラーの番になります");
+            Console.WriteLine("ディーラーは得点が17点以上になるまでカードを引きます");
+            Console.WriteLine("プレイヤーはバーストしたら、ディーラーがカードを引きません\n");
 
-            if (player.GetTotal() < 21)
-            {
-                Console.WriteLine("ディーラーは得点が17点以上になるまでカードを引きます\n");
-            }
-            else
-            {
-                Console.WriteLine("プレイヤーはバーストなので、ディーラーがカードを引きません\n");
-            }
             Console.WriteLine($"裏向きの2枚目のカード：{dealer.Hand[1].Suit}の{dealer.Hand[1].FaceName}");
             Console.WriteLine($"ディーラーの得点：{dealer.GetTotal()}");
-
+            
             while (true)
             {
                 if (dealer.GetTotal() < 17 && player.GetTotal() < 21)
@@ -124,7 +118,7 @@ namespace BlackJack
                 } 
                 else if (dealer.GetTotal() > 17)
                 {
-                    Console.WriteLine("17点以上になったため、ディーラーの番を終了します");
+                    Console.WriteLine("17点以上になったため、ディーラーの番を終了します\n");
                     break;
                 }
                 else 
