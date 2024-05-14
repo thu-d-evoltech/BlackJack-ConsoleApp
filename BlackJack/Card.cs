8002bdd7108e6e486ea7f3b09832995252fc769a
thu-d <thu-d@evoltech.co.jp>
2024-05-14 13:38:27 +0900

    switch文の冗長コードを削除

--- BlackJack/Card.cs
+++ BlackJack/Card.cs
@@ -30,46 +30,6 @@ namespace BlackJack
 
             switch (Face)
             {
-                case CardFace.A:
-                    FaceValue = 1;
-                    FaceName = "A";
-                    break;
-                case CardFace._2:
-                    FaceValue = 2;
-                    FaceName = "2";
-                    break;
-                case CardFace._3:
-                    FaceValue = 3;
-                    FaceName = "3";
-                    break;
-                case CardFace._4:
-                    FaceValue = 4;
-                    FaceName = "4";
-                    break;
-                case CardFace._5:
-                    FaceValue = 5;
-                    FaceName = "5";
-                    break;
-                case CardFace._6:
-                    FaceValue = 6;
-                    FaceName = "6";
-                    break;
-                case CardFace._7:
-                    FaceValue = 7;
-                    FaceName = "7";
-                    break;
-                case CardFace._8:
-                    FaceValue = 8;
-                    FaceName = "8";
-                    break;
-                case CardFace._9:
-                    FaceValue = 9;
-                    FaceName = "9";
-                    break;
-                case CardFace._10:
-                    FaceValue = 10;
-                    FaceName = "10";
-                    break;
                 case CardFace.J:
                     FaceValue = 10;
                     FaceName = "J";
